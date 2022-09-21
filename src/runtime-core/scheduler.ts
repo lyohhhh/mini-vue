98dc656c128665354ec4caf10d93c2fa05e6899c
lyohhhh <lyohhhh@163.com>
2022-09-21 20:55:39 +0800

    chore: 添加布尔值判断是否需要执行微任务

--- src/runtime-core/scheduler.ts
+++ src/runtime-core/scheduler.ts
@@ -1,6 +1,8 @@
 // 同步代码队列
 const queue: Function[] = [];
 
+let isFlushPending = false;
+
 export const queueJobs = (job: Function) => {
   // 不存在再添加
   if (!queue.includes(job)) {
@@ -12,9 +14,12 @@ export const queueJobs = (job: Function) => {
 };
 
 const queueFlush = () => {
+  if (isFlushPending) return;
+  isFlushPending = true;
   // 使用微任务 判断队列中是否有未执行的任务
   // 有的话直接执行
   Promise.resolve().then(() => {
+    isFlushPending = false;
     let job;
     while ((job = queue.shift())) {
       job && job();

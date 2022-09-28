a64d51f1509d12514b272ab7197b23bf1ba1a1d1
lyohhhh <lyohhhh@163.com>
2022-09-28 23:46:37 +0800

    chore: update ast change

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -14,17 +14,41 @@ export const baseParse = (content: string): any => {
   return createRoot(parseChildren(context));
 };
 
+// 实现解析 ast
+const parseInterpolation = (context: Context): Content => {
+  // {{message}}
+  const openDelimiter = "{{";
+  const closeDelimiter = "}}";
+  // 查找到 }} 的位置
+  const closeIndex = context.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  );
+  // 推进两位 排除 {{
+  context.source = context.source.slice(openDelimiter.length);
+  // 减去 }} 两位
+  const rawContentLength = closeIndex - closeDelimiter.length;
+  // 得到 message
+  const content = context.source.slice(0, rawContentLength);
+  // 将已经替换的删除
+  context.source = context.source.slice(
+    rawContentLength + openDelimiter.length
+  );
+
+  return {
+    type: "interpolation",
+    content: {
+      type: "simple_expression",
+      content,
+    },
+  };
+};
+
 // 创建 children
 const parseChildren = (context: Context): Array<Content> => {
-  return [
-    {
-      type: "interpolation",
-      content: {
-        type: "simple_expression",
-        content: "message",
-      },
-    },
-  ];
+  const nodes: Array<Content> = [];
+  nodes.push(parseInterpolation(context));
+  return nodes;
 };
 
 // 创建 ast 根节点

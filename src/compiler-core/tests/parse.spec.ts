2b1f569779a3531721392dbc1b06c307b3ab37cc
lyohhhh <lyohhhh@163.com>
2022-09-30 23:24:12 +0800

    test: add parse element test

--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -16,4 +16,18 @@ describe("Parse", () => {
       });
     });
   });
+
+  describe("element", () => {
+    it("element div", () => {
+      const ast = baseParse("<div></div>");
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.INTERPOLATION,
+        content: {
+          type: NodeTypes.ELEMENT,
+          content: "message",
+          tag: "div",
+        },
+      });
+    });
+  });
 });
